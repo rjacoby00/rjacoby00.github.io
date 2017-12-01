1a4548df224945fa64e4023c755ea21a640daea8
Ryan Jacoby <rjacoby00@icloud.com>
2017-12-01 11:17:13 -0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,14 @@
+importScripts('https://www.gstatic.com/firebasejs/4.7.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.7.0/firebase-messaging.js');
 
+var config = {
+    apiKey: "AIzaSyCoh7ujUlhqVsF6wXDBXH5cmhg0BOS6Wbs",
+    authDomain: "testapp-114be.firebaseapp.com",
+    databaseURL: "https://testapp-114be.firebaseio.com",
+    projectId: "testapp-114be",
+    storageBucket: "testapp-114be.appspot.com",
+    messagingSenderId: "839496366185"
+  };
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
