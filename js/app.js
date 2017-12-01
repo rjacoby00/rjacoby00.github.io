dce19944ac18999310ac7e58f7c76368933dd586
Ryan Jacoby <rjacoby00@icloud.com>
2017-12-01 10:04:50 -0800

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCoh7ujUlhqVsF6wXDBXH5cmhg0BOS6Wbs",
+    authDomain: "testapp-114be.firebaseapp.com",
+    databaseURL: "https://testapp-114be.firebaseio.com",
+    projectId: "testapp-114be",
+    storageBucket: "testapp-114be.appspot.com",
+    messagingSenderId: "839496366185"
+  };
+firebase.initializeApp(config);
